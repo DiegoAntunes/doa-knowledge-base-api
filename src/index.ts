03715171a975535ab03cbbbc0baa907a4d65cf91
Diego Antunes <diegoantunes87@yahoo.com.br>
2025-04-19 17:22:40 -0300

    chore: including middleware for handling global events

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import topicRoutes from './routes/topicRoutes';
 import userRoutes from './routes/userRoutes';
 import resourceRoutes from './routes/resourceRoutes';
+import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
 const PORT = 3000;
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use('/topics', topicRoutes);
 app.use('/users', userRoutes);
 app.use('/resources', resourceRoutes);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
