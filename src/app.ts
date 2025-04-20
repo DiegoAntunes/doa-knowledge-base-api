2c484259471269a635485fbd49a783633f3f103e
Diego Antunes <diegoantunes87@yahoo.com.br>
2025-04-20 09:11:45 -0300

    chore: removing fakeAuthMiddleware from global configuration

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,10 @@ import topicRoutes from './routes/topicRoutes';
 import userRoutes from './routes/userRoutes';
 import resourceRoutes from './routes/resourceRoutes';
 import { errorHandler } from './middlewares/errorHandler';
-import { fakeAuthMiddleware } from './middlewares/fakeAuthMiddleware';
-
 
 const app = express();
 
 app.use(express.json());
-app.use(fakeAuthMiddleware); 
 app.use('/topics', topicRoutes);
 app.use('/users', userRoutes);
 app.use('/resources', resourceRoutes);
